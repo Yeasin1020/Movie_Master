1408243f9d201b44a502af7f5d9f64b7d2b1208f
Yeasin1st <ysbd111@gmail.com>
2023-11-06 13:05:47 +0600

    login page added

--- moviemsater/src/Routes/Routes.jsx
+++ moviemsater/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Home from "../Pages/Home/Home/Home";
 import Main from "../Layout/Main";
+import Login from "../Pages/Login/Login";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
 		{
 			path: '/',
 			element: <Home></Home>
+		},
+		{
+			path: '/login',
+			element: <Login></Login>
 		}
 	]
   },
