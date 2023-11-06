b13dcdf33cdfb7bf7c680c4c557ae8309dd9af7e
Prosenjit Dhar Uzzal <mdeasinsarkar01@gmail.com>
2023-11-06 19:53:39 +0600

    Footer added

--- moviemsater/src/Layout/Main.jsx
+++ moviemsater/src/Layout/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Pages/Footer/Footer';
 import NavBar from '../Shared/NavBar/NavBar';
+import Footer from '../Shared/Footer/Footer';
 
 const Main = () => {
 	return (
