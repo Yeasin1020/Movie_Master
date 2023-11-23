09d75da11d03062c10726207be944ced5d302c14
Prosenjit Dhar Uzzal <mdeasinsarkar01@gmail.com>
2023-11-23 12:15:40 +0600

    slider added

--- moviemsater/src/Pages/Home/Home/Home.jsx
+++ moviemsater/src/Pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Card from "./Card/Card";
 import Slider from "./Slider/Slider";
+import Header from "../../Header/Header";
 
 const Home = () => {
   return (
-  <div>
-    <Slider></Slider>
-    <Card></Card>
-  </div>
+    <div>
+      <Slider></Slider>
+      {/* <Card></Card> */}
+      <Header></Header>
+    </div>
   );
 };
 
