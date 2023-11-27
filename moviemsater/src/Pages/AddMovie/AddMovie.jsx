b704ec3543b90d0e4b43bd2bf4bb5ab6677c8343
Yeasin1st <ysbd111@gmail.com>
2023-11-27 13:43:03 +0600

    addConditionInAddMovie

--- moviemsater/src/Pages/AddMovie/AddMovie.jsx
+++ moviemsater/src/Pages/AddMovie/AddMovie.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./AddMovie.scss";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
 
 const AddMovie = () => {
+
+  const {user} = useContext(AuthContext);
 
   const handleAddMovie = event => {
     event.preventDefault();
@@ -10,11 +13,15 @@ const AddMovie = () => {
     const name = form.name.value;
     const type = form.type.value;
     const category = form.category.value;
+    const Quality = form.Quality.value;
+    const Language = form.Language.value;
+    const hostEmail = form.email.value;
+    const hostName = form.hostName.value;
     const thumbnail = form.thumbnail.value;
     const MovieLink = form.movieLink.value;
     const desc = form.description.value;
-    console.log(name, type, category, thumbnail, MovieLink, desc);
-    const addMovie = { name, type, category, thumbnail, MovieLink, desc }
+    console.log(name, type, category, thumbnail, MovieLink, desc, Quality, Language, hostEmail, hostName);
+    const addMovie = { name, type, category, thumbnail, MovieLink, desc, Quality, Language, hostEmail, hostName }
 
     fetch('http://localhost:5000/addMovie', {
       method: 'POST',
@@ -51,7 +58,7 @@ const AddMovie = () => {
                   id="name"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Type name"
-                  required=""
+                  required
                 />
               </div>
               <div>
@@ -98,6 +105,87 @@ const AddMovie = () => {
                   <option value="War">War</option>
                 </select>
               </div>
+
+              {/* Quality, Language	 */}
+
+              <div>
+                <label
+                  for="Quality"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  	Quality
+                </label>
+                <select
+                  id="Quality"
+                  name="Quality"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
+                  <option selected="">Select Quality</option>
+                  <option value="720p">720p</option>
+                  <option value="1080p">1080p</option>
+                </select>
+              </div>
+              {/* <div class="w-full">
+                  <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                  <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required=""/>
+              </div> */}
+              <div>
+                <label
+                  for="Language"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Language	 
+                </label>
+                <select
+                  id="Language"
+                  name="Language"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                >
+                  <option selected="">Select Language</option>
+                  <option value="Bangla">Bangla</option>
+                  <option value="English">English</option>
+                  <option value="Hindi">Hindi</option>
+                  <option value="Tamil">Tamil</option>
+                 
+                </select>
+              </div>
+              {/* Host Email */}
+              <div class="sm:col-span-2">
+                <label
+                  for="Email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Host Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  value={user?.email}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Host Email "
+                  required
+                />
+              </div>
+              {/* Host Name */}
+              <div class="sm:col-span-2">
+                <label
+                  for=""
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Host Name
+                </label>
+                <input
+                  type="text"
+                  name="hostName"
+                  id="hostName"
+                  value={user?.displayName}
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                  placeholder="Host Name "
+                  required
+                />
+              </div>
+             
               <div class="sm:col-span-2">
                 <label
                   for="thumbnail"
@@ -111,9 +199,11 @@ const AddMovie = () => {
                   id="thumbnail"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Thumbnail Link "
-                  required=""
+                  required
                 />
               </div>
+
+              
               <div class="sm:col-span-2">
                 <label
                   for="name"
@@ -127,7 +217,7 @@ const AddMovie = () => {
                   id="link"
                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Movie Link "
-                  required=""
+                  required
                 />
               </div>
 
@@ -144,6 +234,7 @@ const AddMovie = () => {
                   rows="8"
                   class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                   placeholder="Your description here"
+                  required
                 ></textarea>
               </div>
             </div>
